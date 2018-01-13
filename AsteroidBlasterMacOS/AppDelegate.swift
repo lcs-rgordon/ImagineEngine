c243cb636ac5553cfb72a62b14c18d84ac587fa1
Russell Gordon <rgordon@lcs.on.ca>
2018-01-13 07:51:16 -0500

    Attempt to launch Asteroid Blaster in standalone macOS app

--- AsteroidBlasterMacOS/AppDelegate.swift
+++ AsteroidBlasterMacOS/AppDelegate.swift
@@ -5,22 +5,31 @@
 //  Created by Russell Gordon on 1/13/18.
 //  Copyright © 2018 ImagineEngine. All rights reserved.
 //
-
 import Cocoa
+import ImagineEngine
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
-
+    
+    private var window: GameWindowController!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        let sceneSize = Size(width: 375, height: 667)
+        let gameScene = AsteroidBlasterScene(size: sceneSize)
+        
+        window = GameWindowController(size: sceneSize, scene: gameScene)
+        window.window!.appearance = NSAppearance(named: .vibrantDark)
+        window.window!.title = "Asteroid Blaster"
+        window.window!.makeKeyAndOrderFront(nil)
+        
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
