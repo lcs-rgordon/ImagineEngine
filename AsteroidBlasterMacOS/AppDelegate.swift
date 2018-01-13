381d1a4ded011ee4818f90884bb2702fea90d65d
Russell Gordon <rgordon@lcs.on.ca>
2018-01-13 07:42:16 -0500

    Added target for macOS Cocoa app

---
+++ AsteroidBlasterMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AsteroidBlasterMacOS
+//
+//  Created by Russell Gordon on 1/13/18.
+//  Copyright © 2018 ImagineEngine. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
